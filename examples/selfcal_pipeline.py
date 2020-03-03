a5a93f537653208a52ee45fda913242b8c757727
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2020-03-03 15:40:53 +0200

    Update pipeline: - Predict from fits - Generate primary beam - Transfer model - import fits

--- examples/selfcal_pipeline.py
+++ examples/selfcal_pipeline.py
@@ -1,6 +1,15 @@
 import stimela
 
 PREFIX = "selfcal"
+CASA_PREDICT = True
+PRIMARY_BEAM = True
+# Models to simulate lsm and/or fits
+LSM = "point_skymodel.txt"
+FITS = "selfcal-1-MFS-model.fits"
+# Imaging params
+NPIX = 256
+CELL = "1asec"
+NCHAN = 2
 
 recipe = stimela.Recipe("selfcal_simulation",
                         indir="input",
@@ -19,49 +28,87 @@ recipe.add("simms", "makems", {
     },
     doc="Create Empty MS")
 
-recipe.add("simulator", "simsky", {
-    "msname"               :   recipe.makems.outputs["msname_out"],
-    "config"               :   "tdlconf.profiles",
-    "use_smearing"         :   False,
-    "sefd"                 :   551,  # in Jy
-    "output_column"        :   "DATA",
-    "skymodel"             :   "point_skymodel.txt"
-},
-    doc="Simulate sky model")
+if PRIMARY_BEAM:
+    recipe.add("eidos", "eidos", {
+        "pixels": NPIX,
+        "freq": "1418 1422 2",
+        "diameter": 1.0,
+        "coeff": 'me',
+        "coefficients_file": "meerkat_beam_coeffs_em_zp_dct.npy",
+        "output_eight": False,
+       },
+       doc='Generate primary beam images')
+
+if not CASA_PREDICT:
+    recipe.add("simulator", "simsky", {
+        "msname"               :   recipe.makems.outputs["msname_out"],
+        "config"               :   "tdlconf.profiles",
+        "use_smearing"         :   False,
+        "sefd"                 :   551,  # in Jy
+        "output_column"        :   "DATA",
+        "skymodel"             :   LSM
+        },
+        doc="Simulate sky model")
+
+if CASA_PREDICT:
+    recipe.add('casa_importfits', 'importfits', {
+        "fitsimage": FITS,
+        "imagename": FITS[:-5]+".im",
+        "overwrite": True,
+        },
+        doc='Import fits to casa image')
+
+    recipe.add('casa_ft', 'predict', {
+        "vis": recipe.makems.outputs["msname_out"],
+        "model": recipe.importfits.outputs["image_out"],
+        "nterms": 1,
+        "incremental": False,
+        "usescratch": True,
+        },
+        doc='Predict vis from model')
+
+    recipe.add("simulator", "add_data", {
+        "msname"               :   recipe.predict.outputs["msname_out"],
+        "config"               :   "tdlconf.profiles",
+        "input_column"         :   "MODEL_DATA",
+        "output_column"        :   "DATA",
+        "sim_mode"             :   "add to MS",
+        "sefd"                 :   551,  # in Jy
+        "skymodel"             :   LSM
+        },
+        doc="Add predicted vis into data column with new model")
 
 recipe.add("wsclean", "makeimage1", {
-    "msname"               :   recipe.simsky.outputs["msname_out"],
+    "msname"               :   recipe.add_data.outputs["msname_out"],
     "name"                 :   PREFIX+"-1",
     "datacolumn"           :   "DATA",
     "save_source_list"     :   True,
     "fit_spectral_pol"     :   2,
-    "scale"                :   "1asec",
-    "channels_out"         :   2,
+    "channels_out"         :   NCHAN,
     "join_channels"        :   True,
     "mgain"                :   0.95,
-    "scale"                :   "1.0asec",
+    "scale"                :   CELL,
     "niter"                :   10000,
     "auto_threshold"       :   5,
-    "size"                 :   [256, 256]
-},
+    "size"                 :   [NPIX, NPIX]
+    },
     doc="Image data")
 
 recipe.add("pybdsf", "sourcefinder", {
-    "filename"     :   recipe.makeimage1.outputs["image_out"],
+    "filename"     :   recipe.makeimage1.outputs["restored_image_out"],
     "outfile"      :   "{}-catalog.fits".format(PREFIX),
     "format"       :   "fits",
     "thresh_isl"   :   20,
     "thresh_pix"   :   10,
-},
+    },
     doc="Source finding")
 
 
 recipe.add("bdsf_fits2lsm", "convertfits", {
     "infile"             :   recipe.sourcefinder.outputs["model_out"],
-    "phase_centre_image" :   recipe.makeimage1.outputs["image_out"],
-#    "phase_centre_coord" :   [0.0, -30.0],
+    "phase_centre_image" :   recipe.makeimage1.outputs["restored_image_out"],
     "outfile"            :   "{}-catalog.lsm.html".format(PREFIX)
-},
+    },
     doc="Convert model catalog")
 
 recipe.add("tigger_convert", "convertcatalog", {
@@ -71,7 +118,7 @@ recipe.add("tigger_convert", "convertcatalog", {
     "output_type"        :   "Tigger",
     "type"               :   "auto",
     "rename"             :   True,
-},
+    },
     doc="Convert model catalog")
 
 recipe.add('cubical', "calibration", {
@@ -80,7 +127,7 @@ recipe.add('cubical', "calibration", {
     "out_column"           :   "CORRECTED_DATA",
     "model_lsm"            :   recipe.convertcatalog.outputs["models_out"],
     "model_expression"     :   ["lsm_0"],
-#    "model_column"         :   ['MODEL_DATA"],
+#    "model_column"         :   ['MODEL_DATA"], or use model column instead
 #    "model_expression"     :   ["col_0"],
     "data_time_chunk"      :   24, #128,
     "data_freq_chunk"      :   12, #1024,
@@ -110,25 +157,60 @@ recipe.add('cubical', "calibration", {
     "log_boring"           :   True,
     "shared_memory"        :   4096,
     "montblanc_mem_budget" :   1024,
-},
+    },
     doc="Calibration")
+
 
 recipe.add("wsclean", "makeimage2", {
     "msname"               :   recipe.calibration.outputs["msname_out"],
     "name"                 :   PREFIX+"-2",
     "datacolumn"           :   "CORRECTED_DATA",
     "save_source_list"     :   True,
-    "fit_spectral_pol"     :   2,
     "scale"                :   "1asec",
-    "channels_out"         :   2,
+    "fit_spectral_pol"     :   2,
+    "channels_out"         :   NCHAN,
     "join_channels"        :   True,
     "mgain"                :   0.95,
-    "scale"                :   "1.0asec",
+    "scale"                :   CELL,
     "niter"                :   10000,
     "auto_threshold"       :   5,
-    "size"                 :   [256, 256]
-},
+    "size"                 :   [NPIX, NPIX]
+    },
     doc="Image data")
+
+recipe.add('fitstool', 'makecube1', {
+    "image": recipe.makeimage2.outputs["restored_images_out"],
+    "output": PREFIX+".cube.image.fits",
+    "stack": True,
+    "fits_axis": "FREQ",
+    },
+    doc='Make cube image')
+
+recipe.add('sofia', 'sofia_mask', {
+    "import_inFile": recipe.makecube1.outputs["image_out"],
+    "steps_doFlag": False,
+    "steps_doScaleNoise": True,
+    "steps_doSCfind": True,
+    "steps_doMerge": True,
+    "steps_doReliability": False,
+    "steps_doParameterise": True,
+    "steps_doWriteMask": True,
+    "steps_doMom0": False,
+    "steps_doMom1": False,
+    "steps_doWriteCat": True,
+    "steps_doCubelets": False,
+    "scaleNoise_statistic": 'mad',
+    "SCfind_threshold": 5,
+    "SCfind_rmsMode": 'mad',
+    "merge_radiusX": 3,
+    "merge_radiusY": 3,
+    "merge_radiusZ": 3,
+    "merge_minSizeX": 2,
+    "merge_minSizeY": 2,
+    "merge_minSizeZ": 2,
+    "writeCat_basename": "sofia_mask",
+    },
+    doc='Make SoFiA mask')
 
 recipe.add("crystalball", "transfermodel", {
     "ms": recipe.makeimage2.outputs["msname_out"],
@@ -137,12 +219,32 @@ recipe.add("crystalball", "transfermodel", {
     "row_chunks": 0,
     "model_chunks": 0,
     "points_only": False,
-    "num_sources": 0,
     "num_workers": 8
-},
+    },
     doc="Transfer Model")
 
-recipe.collect_outputs(["makems", "simsky", "makeimage1", "sourcefinder", "calibration", "makeimage2", "transfermodel"])
+recipe.collect_outputs([
+                        "makems",
+                        "makeimage1",
+                        "sourcefinder",
+                        "convertfits",
+                        "convertcatalog",
+                        "calibration",
+                        "makeimage2",
+                        "makecube1",
+                        "sofia_mask",
+                        "transfermodel"
+                       ] + [
+                        "importfits",
+                        "predict",
+                        "add_data"
+                       ] if CASA_PREDICT else [
+                        "simsky",
+                       ] + [
+                        "eidos"
+                       ] if PRIMARY_BEAM else [
+                       ])
 
 recipe.run()
-#recipe.init() # To only generate the cwl files (<name>.cwl  <name>.yml)
+# To only generate the cwl files (<name>.cwl  <name>.yml)
+#recipe.init()
